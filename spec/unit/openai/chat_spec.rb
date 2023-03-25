be2b1fd0f36a95dae58e9dd76b535ee59d39da5e
John Backus <johncbackus@gmail.com>
2023-03-25 19:54:36 -0400

    Add empty test file for OpenAI::Chat

---
+++ spec/unit/openai/chat_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe OpenAI::Chat do
+  subject(:chat) do
+    described_class.new(
+      api: api,
+      settings: { model: 'gpt-3.5-turbo' },
+      messages: []
+    )
+  end
+end
