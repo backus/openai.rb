0651c361114378452d28a8998465d8e80bb533d0
John Backus <johncbackus@gmail.com>
2023-03-25 23:10:40 -0400

    Remove boilerplate from empty test

--- spec/unit/openai/chat_spec.rb
+++ spec/unit/openai/chat_spec.rb
@@ -1,20 +1,4 @@
 # frozen_string_literal: true
 
 RSpec.describe OpenAI::Chat do
-  subject(:chat) do
-    described_class.new(
-      settings: { model: 'gpt-3.5-turbo' },
-      messages: [],
-      openai: instance_double(
-        OpenAI,
-        logger: instance_spy(Logger),
-        api: stubbed_api
-      )
-    )
-  end
-
-  let(:api)        { OpenAI::API.new(api_client)                   }
-  let(:api_client) { OpenAI::API::Client.new('sk-123', http: http) }
-
-  let(:http) { class_spy(HTTP) }
 end
